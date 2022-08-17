0703ed49f14c1be90939a506aee221d9414cd10a
Dale Weber <hybotics.wy@gmail.com>
2022-08-17 11:27:11 -0700

    Updated code (does not work for last row when dimensions are not the same).

--- code/matrix_save.py
+++ code/matrix_save.py
@@ -30,11 +30,10 @@ class Matrix():
     cols = self.cols
     
     print(f"<2> Input array dimensions are {rows} x {cols}")
-    print(f"    Output array should be {cols} x {rows}")
     print()
 
+    # Standard list (1 dimension array)
     if rows == 1:
-      # Standard list (1 dimension array)
       for c in range(cols):
         new_row.append([self.matrix[c]])
         
@@ -42,35 +41,21 @@ class Matrix():
       cols = 1
       new_mat = new_row
     elif cols == 1:
-      # Standard single column
       for r in range(rows):
         new_row.append(self.matrix[r][0])
       
       new_mat = new_row
     else:
-      # This should handle everything else
-      r = 0
-
-      while r < rows:
+      for r in range(rows):
         new_row = []
-        c = 0
 
-        while c < cols:          
+        for c in range(cols):
           print(f"<3> Position = {r} x {c}")
-          
           new_row.append(self.matrix[c][r])
-          
           print(f"<4> ({r}) New Row = {new_row}")
-          
-          c += 1
 
         print()
         new_mat.append(new_row)
-
-        r += 1
-        
-        if r >= rows:
-          break
         
       print(f"<5> New Matrix = {new_mat}")
 
